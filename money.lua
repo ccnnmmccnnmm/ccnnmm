dd3f6125980a6779fe99f51e4ca81f08f38737c5
Xiaoling <1707182940@qq.com>
2026-06-02 03:24:47 +0800

    更新 money.lua

--- money.lua
+++ money.lua
@@ -24,11 +24,7 @@ end
 local availableServers = getAvailableServers()
 
 local targetItems = {
-    "Money Printer", "Golden Rose", "Black Rose", "Spectral Scythe","Blue Candy Cane", "Red Candy Cane", "Heart Balloon", "Golden Clover Balloon", "Bat Balloon",
-    "Suitcase Nuke", "Helicopter Key", "Wild Horse Key", "Easter Basket", "Diamond Banana Peel",
-    "Nuclear Launcher", "Nuclear Missile Launcher", "Trident", "Golden Cup", "Pearl Necklace",
-    "NextBot Grenade”,
-    "Treasure Map", "Snowflake Balloon", "Skull Balloon"
+    "Money Printer", "Golden Rose", "Black Rose", "Spectral Scythe","Blue Candy Cane", "Red Candy Cane", "Heart Balloon", "Golden Clover Balloon", "Bat Balloon", "Suitcase Nuke", "Helicopter Key", "Wild Horse Key", "Easter Basket", "Diamond Banana Peel", "Nuclear Launcher", "Nuclear Missile Launcher", "Trident", "Golden Cup", "Pearl Necklace", "NextBot Grenade”, "Treasure Map", "Snowflake Balloon", "Skull Balloon"
 }
 
 local function checkTimeout()
